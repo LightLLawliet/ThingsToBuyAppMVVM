5895d092e8777c3815ec2226ddb2c8388b605213
NumpyGlumpy <khaypov03@mail.ru>
2023-03-20 01:45:30 +0300

    - 006 a bit of refactoring

--- app/src/main/java/com/example/thingstobuyappmvvm/MainVewModel.kt
+++ app/src/main/java/com/example/thingstobuyappmvvm/MainVewModel.kt
@@ -1,10 +1,6 @@
 package com.example.thingstobuyappmvvm
 
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 
 class MainVewModel(
@@ -30,47 +26,4 @@ class MainVewModel(
 
     override fun observe(owner: LifecycleOwner, observer: Observer<UiState>) =
         communication.observe(owner, observer)
-}
-
-interface MainCommunication {
-
-    interface Put {
-        fun put(value: UiState)
-    }
-
-    interface Observe {
-        fun observe(owner: LifecycleOwner, observer: Observer<UiState>)
-    }
-
-    interface Mutable : Put, Observe
-
-    class Base(private val liveData: MutableLiveData<UiState>) : Mutable {
-        override fun put(value: UiState) {
-            liveData.value = value
-        }
-
-        override fun observe(owner: LifecycleOwner, observer: Observer<UiState>) {
-            liveData.observe(owner, observer)
-        }
-
-    }
-}
-
-sealed class UiState {
-
-    abstract fun apply(daysTextView: TextView, resetButton: Button)
-
-    object ZeroDays : UiState() {
-        override fun apply(daysTextView: TextView, resetButton: Button) {
-            daysTextView.text = "0"
-            resetButton.visibility = View.GONE
-        }
-    }
-
-    data class NDays(private val days: Int) : UiState() {
-        override fun apply(daysTextView: TextView, resetButton: Button) {
-            daysTextView.text = days.toString()
-            resetButton.visibility = View.VISIBLE
-        }
-    }
 }
