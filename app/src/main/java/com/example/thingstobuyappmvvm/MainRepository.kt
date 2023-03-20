45e159b24e6da6184b9514af8b4d7ce541fcffe3
NumpyGlumpy <khaypov03@mail.ru>
2023-03-20 16:16:23 +0300

    -008 repository after test

--- app/src/main/java/com/example/thingstobuyappmvvm/MainRepository.kt
+++ app/src/main/java/com/example/thingstobuyappmvvm/MainRepository.kt
@@ -5,4 +5,37 @@ interface MainRepository {
     fun days(): Int
 
     fun reset()
+
+    class Base(
+        private val cacheDataSource: CacheDataSource,
+        private val now: Now
+    ) : MainRepository {
+
+        override fun days(): Int {
+            val saved = cacheDataSource.time(-1)
+            if (saved == -1L) {
+                reset()
+                return 0
+            } else {
+                val diff = now.time() - saved
+                return (diff / (DAY)).toInt()
+            }
+        }
+
+        companion object {
+            private const val DAY = 24 * 3600 * 1000
+        }
+
+        override fun reset() {
+            cacheDataSource.save(now.time())
+        }
+    }
+}
+
+interface Now {
+    fun time(): Long
+
+    class Base : Now {
+        override fun time() = System.currentTimeMillis()
+    }
 }
--- app/src/test/java/com/example/thingstobuyappmvvm/MainRepositoryTest.kt
+++ app/src/test/java/com/example/thingstobuyappmvvm/MainRepositoryTest.kt
@@ -12,7 +12,7 @@ class MainRepositoryTest {
         val repository = MainRepository.Base(cacheDataSource, now)
         now.addTime(1544)
         val actual = repository.days()
-        val expected: Long = 0
+        val expected = 0
         assertEquals(expected, actual)
         assertEquals(1544, cacheDataSource.time(-1))
     }
@@ -25,7 +25,7 @@ class MainRepositoryTest {
         now.addTime(9 * 24 * 3600 * 1000)
         val repository = MainRepository.Base(cacheDataSource, now)
         val actual = repository.days()
-        val expected: Long = 7
+        val expected = 7
         assertEquals(expected, actual)
     }
 
